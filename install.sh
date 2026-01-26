924b0d3a9ff07b7749084c29c4c0a55bf0c101c3
Romain Ferrali <rferrali@gmail.com>
2026-01-26 14:25:26 +0000

    improve install script

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ miktex --admin packages update-package-database
 miktex --admin packages update
 miktex packages update-package-database
 miktex packages update
+initexmf --admin --update-fndb
 mpm --admin --verbose --package-level=basic --upgrade
 
 # Install required packages for luatex/lualatex format creation
-echo "Installing required packages for luatex formats..."
+echo "Installing required packages for luatex and xetex formats..."
 mpm --admin --install etex
 mpm --admin --install lua-uni-algos
 mpm --admin --install latexmk
+initexmf --admin --update-fndb
 
 # Ensure utf-8.def is resolvable (pdfLaTeX expects utf-8.def while base ships utf8.def)
 UTF_BASE_DIR=/usr/local/share/miktex-texmf/tex/latex/base
